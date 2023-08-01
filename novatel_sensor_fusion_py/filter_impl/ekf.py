e21b09568016febe0217a0d32cc80d6c881eb184
Siyu Chen <siyuchen1996@gmail.com>
2023-08-01 13:25:44 +0200

    ekf still in implementation step

--- novatel_sensor_fusion_py/filter_impl/ekf.py
+++ novatel_sensor_fusion_py/filter_impl/ekf.py
@@ -3,6 +3,10 @@ from scipy.spatial.transform import Rotation
 
 
 class ExtendedKalmanFilter16States:
+    """
+    Implementation based on
+    https://de.mathworks.com/help/fusion/ug/imu-and-gps-fusion-for-inertial-navigation.html
+    """
     def __init__(self, imu_sample_rate, gyro_bias_noise,
                  accelerometer_bias_noise, gyro_noise,
                  accelerometer_noise, init_state=None,
@@ -24,6 +28,9 @@ class ExtendedKalmanFilter16States:
         self.other_additive_noise = other_additive_noise
         self.prev_gyro = None
         self.prev_accel = None
+
+        self.dvel = None
+        self.dangle = None
 
     def get_state(self):
         return self.state
@@ -48,8 +55,81 @@ class ExtendedKalmanFilter16States:
         w[3:6] = scale * self.accelerometer_noise
         return w
 
-    def state_transition_jacobian(self):
-        pass
+    def state_transition_jacobian(self, dt):
+        qw = self.state[0]
+        qx = self.state[1]
+        qy = self.state[2]
+        qz = self.state[3]
+        pe = self.state[4]
+        pn = self.state[5]
+        pu = self.state[6]
+        ve = self.state[7]
+        vn = self.state[8]
+        vu = self.state[9]
+        dthetax_b = self.state[10]
+        dthetay_b = self.state[11]
+        dthetaz_b = self.state[12]
+        dvx_b = self.state[13]
+        dvy_b = self.state[14]
+        dvz_b = self.state[15]
+        
+        dthetax = self.dangle[0]
+        dthetay = self.dangle[1]
+        dthetaz = self.dangle[2]
+        dvx = self.dvel[0]
+        dvy = self.dvel[1]
+        dvz = self.dvel[2]
+
+        dfdx = np.zeros((16, 16))
+        dfdx[0, 0] = 1
+        dfdx[0, 1] = - (dthetax - dthetax_b) / 2
+        dfdx[0, 2] = - (dthetay - dthetay_b) / 2
+        dfdx[0, 3] = - (dthetaz - dthetaz_b) / 2
+        dfdx[0, 10] = qx / 2
+        dfdx[0, 11] = qy / 2
+        dfdx[0, 12] = qz / 2
+        
+        dfdx[1, 0] = (dthetax - dthetax_b) / 2
+        dfdx[1, 1] = 1
+        dfdx[1, 2] = - (dthetaz - dthetaz_b) / 2
+        dfdx[1, 3] = (dthetay - dthetay_b) / 2
+        dfdx[1, 10] = - qw / 2
+        dfdx[1, 11] = - qz / 2
+        dfdx[1, 12] = qy / 2
+
+        dfdx[2, 0] = (dthetay - dthetay_b) / 2
+        dfdx[2, 1] = (dthetaz - dthetaz_b) / 2
+        dfdx[2, 2] = 1
+        dfdx[2, 3] = - (dthetax - dthetax_b) / 2
+        dfdx[2, 10] = qz / 2
+        dfdx[2, 11] = - qw / 2
+        dfdx[2, 12] = - qx / 2
+
+        dfdx[3, 0] = (dthetaz - dthetaz_b) / 2
+        dfdx[3, 1] = - (dthetay - dthetay_b) / 2
+        dfdx[3, 2] = (dthetax - dthetax_b) / 2
+        dfdx[3, 3] = 1
+        dfdx[3, 10] = - qy / 2
+        dfdx[3, 11] = qx / 2
+        dfdx[3, 12] = - qw / 2
+
+        dfdx[4, 4] = 1
+        dfdx[4, 7] = dt
+        dfdx[5, 5] = 1
+        dfdx[5, 8] = dt
+        dfdx[6, 6] = 1
+        dfdx[6, 9] = dt
+
+        dfdx[7, 0] = 2 * qw * (dvx - dvx_b) - 2 * qz * (dvy - dvy_b) + 2 * qy * (dvz - dvz_b)
+        dfdx[7, 1] = 2 * qx * (dvx - dvx_b) + 2 * qy * (dvy - dvy_b) + 2 * qz * (dvz - dvz_b)
+        dfdx[7, 2] = -2 * qy * (dvx - dvx_b) + 2 * qx * (dvy - dvy_b) + 2 * qw * (dvz - dvz_b)
+        dfdx[7, 3] = - 2 * qz * (dvx - dvx_b) - 2 * qw * (dvy - dvy_b) + 2 * qx * (dvz - dvz_b)
+        dfdx[7, 7] = 1
+        dfdx[7, 13] = - (qw ** 2 + qx ** 2 - qy ** 2 - qz ** 2)
+        dfdx[7, 14] = 2 * qw * qz - 2* qx * qy
+        dfdx[7, 15] = - 2 * (qw * qy + qx * qz)
+
+
 
     def predict(self, accel, gyro, dt, trapezoidal_integration=True):
         qw = self.state[0]
@@ -62,9 +142,9 @@ class ExtendedKalmanFilter16States:
         ve = self.state[7]
         vn = self.state[8]
         vu = self.state[9]
-        dax_b = self.state[10]
-        day_b = self.state[11]
-        daz_b = self.state[12]
+        dthetax_b = self.state[10]
+        dthetay_b = self.state[11]
+        dthetaz_b = self.state[12]
         dvx_b = self.state[13]
         dvy_b = self.state[14]
         dvz_b = self.state[15]
@@ -72,7 +152,7 @@ class ExtendedKalmanFilter16States:
         p = np.array([pe, pn, pu])
         v = np.array([ve, vn, vu])
         v_b = np.array([dvx_b, dvy_b, dvz_b])
-        a_b = np.array([dax_b, day_b, daz_b])
+        a_b = np.array([dthetax_b, dthetay_b, dthetaz_b])
 
         if trapezoidal_integration:
             if self.prev_gyro is None:
@@ -81,9 +161,14 @@ class ExtendedKalmanFilter16States:
                 self.prev_accel = accel
             dvel = 0.5 * dt * (self.prev_accel + accel)
             dangle = 0.5 * dt * (self.prev_gyro + gyro)
+            self.prev_accel = accel
+            self.prev_gyro = gyro
         else:
             dvel = accel * dt
             dangle = gyro * dt
+        
+        self.dvel = dvel
+        self.dangle = dangle
 
         # R = np.zeros((3, 3))
         # R[0, 0] = qw ** 2 + qx ** 2 - qy ** 2 - qz ** 2
--- scripts/novatel_raw_data_analysis/bestgnsspos_bestpos.py
+++ scripts/novatel_raw_data_analysis/bestgnsspos_bestpos.py
@@ -1,3 +1,4 @@
+from gps_time import GPSTime
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -6,6 +7,8 @@ def plot_bestgnsspos_bestpos(abs_path, file_name):
     bestgnsspos = []
     bestpos = []
     gnss_std = []
+    bestgnsspos_ts = []
+    bestpos_ts = []
     with open(abs_path + file_name) as data_file:
         content = data_file.read().split('\n')
         for index, line in enumerate(content):
@@ -16,6 +19,10 @@ def plot_bestgnsspos_bestpos(abs_path, file_name):
                 lon = float(data[3])
                 h = float(data[4])
                 bestpos.append([lat, lon, h])
+
+                header = msg[0].split(',')
+                cur_time = GPSTime(week_number=int(header[5]), time_of_week=float(header[6]))
+                bestpos_ts.append(cur_time)
             if 'BESTGNSSPOSA' in line:
                 msg = line.split(';')
                 data = msg[1].split(',')
@@ -29,30 +36,49 @@ def plot_bestgnsspos_bestpos(abs_path, file_name):
                 gnss_std.append([lat_std, lon_std, h_std])
 
                 bestgnsspos.append([lat, lon, h])
-    return np.array(bestgnsspos), np.array(bestpos), np.array(gnss_std)
+
+                header = msg[0].split(',')
+                cur_time = GPSTime(week_number=int(header[5]), time_of_week=float(header[6]))
+                bestgnsspos_ts.append(cur_time)
+    return np.array(bestgnsspos), np.array(bestpos), np.array(gnss_std), bestgnsspos_ts, bestpos_ts
+
+
+def search_specific_gps_time(gps_time_array, time_of_week):
+    for index in range(len(gps_time_array) - 1):
+        if gps_time_array[index].time_of_week <= time_of_week and \
+            gps_time_array[index + 1].time_of_week >= time_of_week:
+            return index
 
 
 folder_path = '/home/siyuchen/Documents/Novatel_Stereocam_Daten_20230703/20230703_140222/'
 file_name = 'IMUData_20230703_140222.log'
-bestgnsspos, bestpos, gnss_std = plot_bestgnsspos_bestpos(folder_path, file_name)
+bestgnsspos, bestpos, gnss_std, bestgnsspos_ts, bestpos_ts = plot_bestgnsspos_bestpos(folder_path, file_name)
 # print(bestgnsspos.shape)
 print(gnss_std.shape)
+print(bestgnsspos_ts[9080])
+start_id = search_specific_gps_time(bestgnsspos_ts, time_of_week=130801)
+end_id = search_specific_gps_time(bestgnsspos_ts, time_of_week=130911)
+print(start_id, end_id)
 
 fig, axs = plt.subplots(3, 1)
 axs[0].set_title('Latitude')
 axs[0].plot(bestgnsspos[:, 0], label='bestgnsspos')
 axs[0].plot(bestpos[:, 0], label='bestpos')
-axs[0].legend()
+axs[0].plot(start_id + np.array(list(range(end_id - start_id))), \
+            bestgnsspos[start_id:end_id, 0], label='selected area')
 par_0 = axs[0].twinx()
-par_0.plot(gnss_std[:, 0], 'g', label='latitude std')
-par_0.legend()
+par_0.plot(gnss_std[:, 0], 'r', label='latitude std')
+# par_0.legend()
+axs[0].legend()
 axs[0].ticklabel_format(useOffset=False)
 
 axs[1].set_title('Longitude')
 axs[1].plot(bestgnsspos[:, 1], label='bestgnsspos')
 axs[1].plot(bestpos[:, 1], label='bestpos')
+axs[1].plot(start_id + np.array(list(range(end_id - start_id))), \
+            bestgnsspos[start_id:end_id, 1], label='selected area')
 par_1 = axs[1].twinx()
-par_1.plot(gnss_std[:, 1], 'g', label='longitude std')
+par_1.plot(gnss_std[:, 1], 'r', label='longitude std')
 par_1.legend()
 axs[1].ticklabel_format(useOffset=False)
 axs[1].legend()
@@ -60,8 +86,10 @@ axs[1].legend()
 axs[2].set_title('Altitude')
 axs[2].plot(bestgnsspos[:, 2], label='bestgnsspos')
 axs[2].plot(bestpos[:, 2], label='bestpos')
+axs[2].plot(start_id + np.array(list(range(end_id - start_id))), \
+            bestgnsspos[start_id:end_id, 2], label='selected area')
 par_2 = axs[2].twinx()
-par_2.plot(gnss_std[:, 2], 'g', label='altitude std')
+par_2.plot(gnss_std[:, 2], 'r', label='altitude std')
 par_2.legend()
 axs[2].ticklabel_format(useOffset=False)
 axs[2].legend()
