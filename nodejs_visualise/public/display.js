b23dde4e671ebc8a880c0079940c02335125ff05
Siyu Chen <siyuchen1996@gmail.com>
2023-08-18 15:58:07 +0200

    disable visualisation works, but different colors doesn't

--- nodejs_visualise/public/display.js
+++ nodejs_visualise/public/display.js
@@ -4,6 +4,16 @@ let accumulated_error = 0.0;
 let accumulated_nb = 0;
 let display_plot = true;
 let is_start_init = false;
+let display_ground_truth = true;
+let display_raw_gnss = true;
+let display_fused = true;
+const polyline_width = 4;
+
+// https://www.simplifiedsciencepublishing.com/resources/best-color-palettes-for-scientific-figures-and-data-visualizations
+// https://rgbacolorpicker.com/hex-to-rgba
+// dark red: rgba(193, 39, 45, 0.4), light_red: rgba(193, 39, 45, 0.2), green: rgba(0, 129, 118, 1)
+const dark_red = '#c1272d66', light_red = '#c1272d33', green = '#008176';
+const coral = '#FF7F5077', teal = '#008080', goldenrod = '#DAA52099';
 
 let options = {
     plugins: {
@@ -229,7 +239,7 @@ let up_plot = {
         }]
 };
 
-function convert_ros_timestamp_to_float(ros_ts){
+function convert_ros_timestamp_to_float(ros_ts) {
     const seconds = parseInt(ros_ts.nanosec) / 1e9;
     return parseInt(ros_ts.sec) + seconds;
 }
@@ -243,7 +253,7 @@ function addData(chart, xValue, yValue, index) {
 
 function updateXAxis(chart) {
     chart.options.scales.x.min = windowStart;
-    chart.options.scales.x.max = windowStart + 5;
+    chart.options.scales.x.max = windowStart + update_time_interval;
     chart.update({
         mode: 'none'
     });
@@ -252,6 +262,32 @@ function updateXAxis(chart) {
         // clear array and allow garbage collection
         chart.data.datasets[index].data = []
     }
+}
+
+function modifyPolylineStyle(map, polyline, decorator) {
+    if (map.hasLayer(polyline)) {
+        map.removeLayer(polyline)
+        // console.log("The polyline is on the map.");
+    } else {
+        // console.log("The polyline is not on the map.");
+    }
+
+    if (map.hasLayer(decorator)) {
+        map.removeLayer(decorator);
+        // console.log("The decorator is on the map.");
+    } else {
+        // console.log("The decorator is not on the map.");
+    }
+
+    polyline = L.polyline(polyline.getLatLngs(), polyline.options).addTo(map);
+    // console.log(polyline.getLatLngs())
+    decorator = L.polylineDecorator(polyline, {
+            patterns: [
+                {offset: '0%', endOffset: '50%', repeat: 0, symbol: L.Symbol.dash({pathOptions: {color: 'red', weight: polyline_width}})},
+                {offset: '50%', endOffset: '100%', repeat: 0, symbol: L.Symbol.dash({pathOptions: {color: 'blue', weight: polyline_width}})}
+            ]
+        }).addTo(map);
+    return [polyline, decorator]
 }
 
 L.Control.CustomButton = L.Control.extend({
@@ -297,7 +333,7 @@ L.Control.Label = L.Control.extend({
     // Update the label's content
     update: function(rms_value, nb) {
         this._div.innerHTML = '<h4>Statistical</h4>';
-        this._div.innerHTML += `<i>RMS Value = ${rms_value}</i><br>`;
+        this._div.innerHTML += `<i>RMS Value = ${rms_value} m</i><br>`;
         this._div.innerHTML += `<i>Count of received values = ${nb}</i><br>`;
     }
 });
@@ -353,8 +389,15 @@ document.addEventListener('DOMContentLoaded', () => {
             updateXAxis(up_chart);
             let rms = Math.sqrt(accumulated_error / accumulated_nb);
             label_rms.update(rms.toFixed(3), accumulated_nb);
-            console.log('nb', accumulated_nb)
+            // console.log('nb', accumulated_nb)
         }
+        [ref_polyline, ref_decorator] = modifyPolylineStyle(map, ref_polyline, ref_decorator);
+        // map.removeLayer(ref_polyline);
+        // if (decorator){
+        //     map.removeLayer(decorator)
+        // }
+        // map.addLayer(ref_polyline);
+        //
     }, update_time_interval * 1000);
 
 
@@ -362,6 +405,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let nb_cycle = 100;
     let cur_id = 0;
     let cur_polyline, ref_polyline, fused_polyline;
+    let cur_decorator, ref_decorator, fused_decorator;
     let ref_lat = 50.77766817103, ref_lon = 6.07832598964;
 
     // Create a map centered on a default location
@@ -375,24 +419,66 @@ document.addEventListener('DOMContentLoaded', () => {
 
     L.control.customButton().addTo(map);
 
-    // https://www.simplifiedsciencepublishing.com/resources/best-color-palettes-for-scientific-figures-and-data-visualizations
-    // https://rgbacolorpicker.com/hex-to-rgba
-    // dark red: rgba(193, 39, 45, 0.4), light_red: rgba(193, 39, 45, 0.2), green: rgba(0, 129, 118, 1)
-    var dark_red = '#c1272d66', light_red = '#c1272d33', green = '#008176';
-    cur_polyline = L.polyline([], {color: light_red, weight: 5}).addTo(map);
-    ref_polyline = L.polyline([], {color: dark_red, weight: 4}).addTo(map);
-    fused_polyline = L.polyline([], {color: green, weight: 4}).addTo(map);
+    cur_polyline = L.polyline([], {color: coral, weight: polyline_width}).addTo(map);
+    ref_polyline = L.polyline([], {color: teal, weight: polyline_width}).addTo(map);
+    fused_polyline = L.polyline([], {color: goldenrod, weight: polyline_width}).addTo(map);
 
     // https://codepen.io/haakseth/pen/KQbjdO
     /*Legend specific*/
-    var legend = L.control({ position: "bottomleft" });
+    let legend = L.control({ position: "bottomleft" });
 
     legend.onAdd = function(map) {
-        var div = L.DomUtil.create("div", "legend");
+        let div = L.DomUtil.create("div", "legend");
         div.innerHTML += "<h4>Legend</h4>";
-        div.innerHTML += `<i style="background: ${light_red}"></i><span>GNSS Pose</span><br>`;
-        div.innerHTML += `<i style="background: ${dark_red}"></i><span>Grund Truth</span><br>`;
-        div.innerHTML += `<i style="background: ${green}"></i><span>Fused</span><br>`;
+
+        // GNSS Pose
+        let gnssPose = L.DomUtil.create("div", "", div);
+        gnssPose.innerHTML = `<i style="background: ${cur_polyline.options.color}"></i><span>GNSS Pose</span>`;
+        gnssPose.addEventListener("click", function() {
+            // console.log("GNSS Pose clicked");
+            // Your callback function here
+            display_raw_gnss = !display_raw_gnss;
+            if (display_raw_gnss){
+                this.style.opacity = 1;
+                map.addLayer(cur_polyline);
+            } else{
+                this.style.opacity = 0.5;
+                map.removeLayer(cur_polyline);
+            }
+        });
+
+        // Ground Truth
+        let groundTruth = L.DomUtil.create("div", "", div);
+        groundTruth.innerHTML = `<i style="background: ${ref_polyline.options.color}"></i><span>Ground Truth</span>`;
+        groundTruth.addEventListener("click", function() {
+            // console.log("Ground Truth clicked");
+            // Your callback function here
+            display_ground_truth = !display_ground_truth;
+            if (display_ground_truth){
+                this.style.opacity = 1;
+                map.addLayer(ref_polyline);
+            } else{
+                this.style.opacity = 0.5;
+                map.removeLayer(ref_polyline);
+            }
+        });
+
+        // Fused
+        let fused = L.DomUtil.create("div", "", div);
+        fused.innerHTML = `<i style="background: ${fused_polyline.options.color}"></i><span>Fused</span>`;
+        fused.addEventListener("click", function() {
+            // console.log("Fused clicked");
+            // Your callback function here
+            display_fused = !display_fused;
+            if (display_fused){
+                this.style.opacity = 1;
+                map.addLayer(fused_polyline);
+            } else{
+                this.style.opacity = 0.5;
+                map.removeLayer(fused_polyline);
+            }
+        });
+
         return div;
     };
 
@@ -401,7 +487,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let label_rms = new L.Control.Label({ position: 'bottomright' });
     label_rms.addTo(map);
 
-    var cur_mark = L.marker([ref_lat, ref_lon]).addTo(map);
+    let cur_mark = L.marker([ref_lat, ref_lon]).addTo(map);
     cur_mark.bindPopup('Reference location')
         .openPopup();
 
@@ -433,11 +519,11 @@ document.addEventListener('DOMContentLoaded', () => {
             
             setTimeout(function() {
                 temp_marker.remove();
-            }, 20000);
+            }, 5000);
 
-            cur_polyline.setStyle({ color: light_red });
+            // cur_polyline.setStyle({ color: coral });
             map.fitBounds(cur_polyline.getBounds());
-            cur_polyline = L.polyline([], {color: dark_red, weight: 5}).addTo(map);
+            // cur_polyline = L.polyline([], {color: coral, weight: 4}).addTo(map);
             cur_id = 0;
         }
     });
@@ -476,11 +562,9 @@ document.addEventListener('DOMContentLoaded', () => {
             start = convert_ros_timestamp_to_float(stamp);
         }
 
-        const now = convert_ros_timestamp_to_float(stamp);
-        const duration = now - start;
-
-        console.log('display_plot', display_plot);
         if (display_plot){
+            const now = convert_ros_timestamp_to_float(stamp);
+            const duration = now - start;
             addData(north_chart, duration, Math.abs(msg.vector.y), 0);
             addData(east_chart, duration, Math.abs(msg.vector.x), 0)
             addData(up_chart, duration, Math.abs(msg.vector.z), 0)
